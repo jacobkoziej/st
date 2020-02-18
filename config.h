5fb2570716bcbee0bc87fa52ecf85af4992ed1ea
Jacob Koziej <jacobkoziej@gmail.com>
2020-02-17 22:22:52 -0500

    Increase default font size
    Increase default font size to 18

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fura Code Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fura Code Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
